83bad35b3d2d0fb3558c35b0b566890a7d2cdd3c
Igor Grosu <igrgro@gmail.com>
2023-11-28 17:34:22 +0200

    deployed

--- quiz/settings.py
+++ quiz/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['owngame.pythonanywhere.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -85,7 +85,7 @@ USE_I18N = True
 
 USE_TZ = True
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
